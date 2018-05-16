67a90c6bbff6574159b98b88c0aea301dac22ab6
Anton Strömkvist <anton@stromkvist.com>
2018-05-16 18:39:31 +0200

    Haskell: Add fifth iteration of Collatz Conjecture
    Using Haskell's laziness and the `Data.List.iteration` function to do
    some neat stuff.

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,13 +2,14 @@ module CollatzConjecture
   ( collatz
   ) where
 
-import Data.Maybe (Maybe(Just, Nothing))
+import Data.List (iterate, genericLength)
 
 collatz :: Integer -> Maybe Integer
 collatz n
   | n <= 0    = Nothing
-  | n == 1    = Just 0
-  | even n    = next (n `div` 2)
-  | otherwise = next (3 * n + 1)
-  where
-    next = fmap succ . collatz
+  | otherwise = Just $ genericLength $ takeWhile ((/=) 1) $ iterate next n
+
+next :: Integer -> Integer
+next n
+  | even n    = n `div` 2
+  | otherwise = 3 * n + 1
