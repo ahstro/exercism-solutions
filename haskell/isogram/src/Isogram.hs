5c987e1a915745b84d99c54eec06db290af784c5
Anton Strömkvist <anton@stromkvist.com>
2019-03-19 11:50:46 +0100

    Haskell: Add second iteration of Isogram
    Use `Data.Char.isAlpha` instead of custom `allowed`

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -2,11 +2,10 @@ module Isogram
   ( isIsogram
   ) where
 
-import           Data.Char (toLower)
+import           Data.Char (isAlpha, toLower)
 import           Data.List (filter, nub)
 
 isIsogram :: String -> Bool
 isIsogram xs = normalized == nub normalized
   where
-    allowed c = notElem c [' ', '-']
-    normalized = map toLower $ filter allowed xs
+    normalized = map toLower $ filter isAlpha xs
